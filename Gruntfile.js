42620543159d85cc8f86d8b3ead7e68e432b5a9d
xiapeimin <2536733090@qq.com>
2020-04-04 20:32:16 +0800

    pub

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+/* global module: true */
+module.exports = function (grunt) {
+  grunt.initConfig({
+    htmlhint: {
+      options: {
+        htmlhintrc: '.htmlhintrc'
+      },
+      src: 'index.html'
+    },
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      src: 'css/index.css'
+    },
+    eslint: {
+      options: {
+        configFile: '.eslintrc.json'
+      },
+      target: ['./js/*.js', './component/*.js']
+    },
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    concat: {
+      js: {
+        src: ['js/*.js', 'component/*.js'],
+        dest: 'dist/bundle.js'
+      }   
+    },
+    uglify: {
+      'dist/bundle.min.js': 'dist/bundle.js'
+    },
+    cssmin: {
+      'dist/bundle.min.css': './css/index.css'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-eslint');
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+  grunt.registerTask('build', ['concat', 'uglify', 'cssmin',  'htmlmin']);
+};
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# mynote
+# jsnotepad
